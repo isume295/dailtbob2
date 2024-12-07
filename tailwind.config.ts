ed0d17255b09b9f34af198a60082992e844b1495
Mubarak Adem <mubaarakadem@gmail.com>
2024-12-07 07:56:04 +0300

    remove files from main

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,28 +8,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        noto: ["Noto Sans KR", "sans-serif"],
-      },
-
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        mainBlack: "#4D4D4D",
-        mainBlue: "#007AFF",
-        mainWhite: "#ffffff",
-        mainPurple: "#9388FE",
-        mainGray: "#A1A9A3",
-        secondGray: "#A0A0A0",
-        thirdGray:"#DCDCDC",
-        bgPurple: "#F6F5FF",
-        bgGray: "#F5F6F8",
-        grayBorder: "#D2D5D6",
-      },
-    },
-  },
+  
   darkMode: "class",
   plugins: [nextui()],
 };
