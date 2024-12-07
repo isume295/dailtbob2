de02e143272a4a9201185773d44b6752e31fb3f3
Mubarak Adem <mubaarakadem@gmail.com>
2024-12-07 08:56:29 +0300

    add Point conversion history page

--- src/app/(pages)/admin/(정산 관리)/settlement/settlement-management/(포인트 전환 내역)/Point-conversion-history/page.tsx
+++ src/app/(pages)/admin/(정산 관리)/settlement/settlement-management/(포인트 전환 내역)/Point-conversion-history/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import CommonHeader from '@/components/CommonHeader'
 import DropDownWithLabel from '@/components/DropDownWithLabel'
+import DropDown from '@/components/DropDown'
+import HeaderDropDown from '@/components/HeaderDropDown'
 import InputWithLabel from '@/components/InputWithLabel'
-import { Button } from '@nextui-org/react'
-import React from 'react'
+import InputNoLabel from '@/components/InputNoLable'
+import { Button, Checkbox, Link, Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
+import React, { useState } from 'react'
+import row10Column1 from '@/data/tables/row10Column1'
 
 const page = () => {
   const labelStyle = "font-normal text-base text-mainBlack min-w-[90px]";
@@ -13,6 +17,29 @@ const page = () => {
     { key: "option3", label: "전체 회원" },
   ];
   const defaultDropDown=dropDownOptions[0].key
+  const viewOptions = [
+    { key: "10", label: "10개씩 보기" },
+    { key: "20", label: "20개씩 보기" },
+    { key: "50", label: "50개씩 보기" },
+    { key: "100", label: "100개씩 보기" },
+  ];
+  const [currentData, setCurrentData] = useState<any>();
+  const viewOptionsDefault = viewOptions[0].key;
+  const [viewValue, setViewValue] = useState(viewOptionsDefault);
+  const [page, setPage] = useState(1);
+
+  const rowsPerPage = parseInt(viewValue);
+  const pages = Math.ceil(row10Column1.length / rowsPerPage);
+
+  // Memoized items
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    return row10Column1.slice(start, start + rowsPerPage);
+  }, [page, rowsPerPage]);
+  const [clickedRowIds, setClickedRowIds] = useState<number[]>([]);
+  const [allListCheckedPageNumbers, setAllListCheckedPageNumbers] = useState<
+    number[]
+  >([]);
   return (
     <section>
     <CommonHeader title='포인트 전환 내역 '/>
@@ -35,6 +62,131 @@ const page = () => {
         defaultSelectedKeys={defaultDropDown} title='상태' titleStyles={labelStyle} selectStyles='w-[470px]'/>
       </div>
       </header>
+      <header className= " bg-white py-10 px-10 rounded-[20px] mt-5">
+      <div className='flex items-center justify-between'>
+          
+      
+          <p className="font-bold text-mainBlack">총 00건</p>
+          <div className="flex items-center gap-3">
+          <DropDown options={dropDownOptions} defaultSelectedKeys={defaultDropDown} selectStyles="w-[124px]"/>
+            <HeaderDropDown
+              options={viewOptions}
+              defaultSelectedKey={viewOptionsDefault}
+              value={viewValue}
+              setNewValue={setViewValue}
+              styles="w-[100px] "
+              mainStyles="bg-transparent border border-grayBorder rounded-[5px]"
+            />
+            <Button className="py-3 px-5 rounded-md bg-grayLight text-white"
+            
+            >
+              삭제
+            </Button>
+
+          </div>
+        
+        </div>
+        <article className="mt-10">
+        <Table
+  aria-label="Data Table"
+  shadow="none"
+  className="table-fixed w-full" // Ensures the table layout doesn't expand
+  classNames={{
+    th: [
+      "font-normal text-[16px] bg-[#EEEEEE] text-[#A1A9A3] h-[48px] text-center whitespace-nowrap",
+    ],
+    td: [
+      "px-6 text-center font-normal text-base text-[#363941] whitespace-nowrap truncate max-w-[150px]", // Truncate text and limit width
+    ],
+  }}
+  bottomContent={
+    <div className="flex w-full justify-center mt-8">
+      <Pagination
+        isCompact
+        showControls
+        showShadow
+        color="secondary"
+        page={page}
+        total={pages}
+        onChange={(page) => setPage(page)}
+      />
+    </div>
+  }
+>
+  <TableHeader>
+    <TableColumn className="w-[5%]">
+    <Checkbox
+    onClick={() => {
+      if (allListCheckedPageNumbers.includes(page)) {
+        setAllListCheckedPageNumbers(
+          allListCheckedPageNumbers.filter((number) => number !== page)
+        );
+        setClickedRowIds(
+          clickedRowIds.filter(
+            (number) => !items.map((item: any) => item.number).includes(number)
+          )
+        );
+      } else {
+        setClickedRowIds([
+          ...clickedRowIds,
+          ...items.map((item: any) => item.number),
+        ]);
+        setAllListCheckedPageNumbers([
+          ...allListCheckedPageNumbers,
+          page,
+        ]);
+      }
+    }}
+    className="size-[14px] rounded-[2px] bg-transparent"
+    isSelected={allListCheckedPageNumbers.includes(page)}
+  />
+    </TableColumn>
+    <TableColumn className="w-[10%]">번호</TableColumn>
+    <TableColumn className="w-[10%]">사용자 ID</TableColumn>
+    <TableColumn className="w-[10%]">이름</TableColumn>
+    <TableColumn className="w-[10%]">전환 포인트</TableColumn>
+    <TableColumn className="w-[20%]">네이버 포인트</TableColumn>
+    <TableColumn className="w-[10%]">전환 결과</TableColumn>
+    <TableColumn className="w-[10%]">상세보기</TableColumn>
+  </TableHeader>
+  <TableBody>
+    {items.map((row) => (
+      <TableRow key={row.number} className="border-b-1">
+        <TableCell>
+          <Checkbox
+            className="text-center size-[14px] rounded-[2px]"
+            onClick={() => {
+              if (clickedRowIds.includes(row.number)) {
+                setClickedRowIds(
+                  clickedRowIds.filter((id) => id !== row.number)
+                );
+              } else {
+                setClickedRowIds([...clickedRowIds, row.number]);
+              }
+            }}
+            isSelected={clickedRowIds.includes(row.number)}
+          />
+        </TableCell>
+        <TableCell className="truncate">{row.number}</TableCell>
+        <TableCell className="truncate">{row.userId}</TableCell>
+        <TableCell className="truncate">{row.name}</TableCell>
+        <TableCell className="truncate">{row.turningPoints}</TableCell>
+        <TableCell className="truncate">
+          
+            {row.naverPoints}
+          
+        </TableCell>
+        <TableCell className="truncate text-[#248DEE]">{row.conversionResults}</TableCell>
+        <TableCell className="truncate"> <Link href='' className='underline underline-offset-1' >{row.viewDetails}</Link></TableCell>
+
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+
+        </article>
+      </header>
+      
       </section>
   )
 }
--- src/data/tables/row10Column1.ts
+++ src/data/tables/row10Column1.ts
@@ -1,16 +1,19 @@
 const demo = [
   {
     number: 1,
-    question: "Q. 제목 영역 텍스트 제목 영역 텍스트 제목 영역 텍스트 ",
-    answer: "A. 제목 영역 텍스트 제목 영역 텍스트 제목 영역 텍스트",
-    viewDetails: "상세보기",
+    userId: "abcd123@naver.com ",
+    name: "홍길동",
+    turningPoints: "3,000",
+    naverPoints:"3,000",
+    conversionResults:"성공",
+    viewDetails:"실패 사유"
+
   },
 ];
 
 const row10Column1 = [
   ...Array.from({ length: 70 }, (_, index) => ({
     ...demo[0],
-    id: index + 1,
     number: index + 1,
   })),
 ];
